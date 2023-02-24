f07e6ce0af3b8f3502361b75358db51bce0a3693
sophiawithph <sophiasgs11@gmail.com>
2023-02-24 11:06:06 -0300

    testes com twig

--- produto.php
+++ produto.php
@@ -4,11 +4,32 @@
 
 $loader = new \Twig\Loader\FilesystemLoader('./templates');
 
-
 $twig = new \Twig\Environment ($loader);
 
-$templates =$twig -> load ('produtos.html');
+$templates = $twig -> load('produtos.html');
+
+
+$produtos = [
+    [
+        'nome' => 'chinelo',
+        'preco' => 30
+    ],
+    [
+        'nome' => 'camiseta',
+        'preco' => 50
+    ],[
+        'nome' => 'boné',
+        'preco' => 39.9
+    ],
+    [
+        'nome' => 'calça',
+        'preco' => 150.00
+    ],
+];
+
+
 echo $templates -> render([
     
-    'titulo' => 'produtos'
+    'titulo' => 'produtos', 
+    'produtos'=> $produtos,
 ]);
